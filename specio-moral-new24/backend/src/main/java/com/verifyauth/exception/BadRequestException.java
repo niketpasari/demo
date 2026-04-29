6e508d5286b24aa647e96d0e89b5db0aa365d127
niketpasari <72893216+niketpasari@users.noreply.github.com>
2026-04-29 14:27:37 +0530

    Delete specio-moral-new24 directory

--- specio-moral-new24/backend/src/main/java/com/verifyauth/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.verifyauth.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- specio-moral-new24/backend/src/main/java/com/verifyauth/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.verifyauth.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
